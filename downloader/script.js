0b424dcc8989d14321d3bd9e5ea75a97ee019118
hoto17296 <mail@hoto.me>
2018-09-22 09:44:52 +0900

    invoke lambda

---
+++ downloader/script.js
@@ -0,0 +1,11 @@
+function main() {
+  const lambda = new AWS.Lambda();
+  const params = {
+    FunctionName : 'depot-backend',
+    Payload : JSON.stringify({ password: 'foo' }),
+  };
+  lambda.invoke(params, (err, data) => {
+    if (err) console.error(err, err.stack);
+    else console.log(JSON.parse(data.Payload));
+  });
+}
--- lambda/index.js
+++ lambda/index.js
@@ -1,3 +1,12 @@
-exports.handler = async (event) => {
-    return { message: 'Yay!' };
+const crypto = require('crypto');
+
+function hash(str, encoding='hex') {
+    const hmac = crypto.createHmac('sha256', 'deadbeef');
+    hmac.update(str);
+    return hmac.digest(encoding);
 }
+
+exports.handler = async (event) => {
+    event.hash = hash(event.password);
+    return event;
+};
